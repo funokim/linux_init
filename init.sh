a892c9986f566d1c3a54e3ce08ce3b6f0217eb32
Hun-ho Kim <funokim425@gmail.com>
2021-05-30 11:04:20 +0200

    Added zsh, oh-my-zsh, powerlevel10k, variety, plank, ulauncher, visual studio code, python-is-python3

--- init.sh
+++ init.sh
@@ -1,5 +1,31 @@
-# Google Chrome Installation
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
+# Google Chrome
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo dpkg -i google-chrome-stable_current_amd64.deb
+#rm google-chrome-stable_current_amd64.deb
+
+# Zsh
+#sudo apt install zsh
+#chsh -s $(which zsh)
+
+# Oh-my-zsh
+#sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Powerlevel10k
+#wget -P ./manual/ https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
+
+# Variety, Plank
+#sudo apt install variety plank
+
+# Ulauncher
+#wget https://github.com/Ulauncher/Ulauncher/releases/download/5.11.0/ulauncher_5.11.0_all.deb
+#rm ulauncher_5.11.0_all.deb
+
+# Visual Studio Code
+#snap install code --classic
+
+# Python (python-is-python3)
+#sudo apt install python-is-python3
+
 
