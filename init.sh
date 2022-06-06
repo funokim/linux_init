a86c0b3105ee36a167a4d0ad79e18237c5d06cc7
Hun-ho Kim <hunho.kim425@gmail.com>
2022-06-06 20:20:26 +0900

    Add autojump

--- init.sh
+++ init.sh
@@ -8,3 +8,7 @@ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O 
 wget -P ./input/ https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
+
+# Autojump
+sudo apt install autojump
+echo ". /usr/share/autojump/autojump.sh" >> ~/.zshrc                                                                              
